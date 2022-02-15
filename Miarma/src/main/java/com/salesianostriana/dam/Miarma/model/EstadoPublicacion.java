affcb788f61b9a95a4827d78092fddd2e72139cb
vicente <etenciv@gmail.com>
2022-02-15 14:03:04 +0100

    arreglos inma2

--- Miarma/src/main/java/com/salesianostriana/dam/Miarma/model/EstadoPublicacion.java
+++ Miarma/src/main/java/com/salesianostriana/dam/Miarma/model/EstadoPublicacion.java
@@ -2,5 +2,14 @@ package com.salesianostriana.dam.Miarma.model;
 
 public enum EstadoPublicacion {
 
-    PUBLICO, PRIVADO
+    PUBLICO("PUBLICO"), PRIVADO("PRIVADO");
+
+    private String valor;
+
+    private EstadoPublicacion(String valor){this.valor= valor;}
+
+    public String getValor(){return valor;}
+
+    public void setValor(String valor){this.valor= valor;}
+
 }
