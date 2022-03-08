4f7c48a531de3904090a4469636b236226df5ded
vicente <etenciv@gmail.com>
2022-03-08 13:05:21 +0100

    CORRECCION FINAL

--- Miarma/src/main/java/com/salesianostriana/dam/Miarma/security/SecurityConfig.java
+++ Miarma/src/main/java/com/salesianostriana/dam/Miarma/security/SecurityConfig.java
@@ -93,7 +93,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
